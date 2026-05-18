6d9c66eb3802d3c50fcb1f8cc5234d9364375c5e
baglan-nurgali <251667@astanait.edu.kz>
2026-05-18 08:32:20 +0500

    Update Experiment.java

--- Experiment.java
+++ Experiment.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 
 public class Experiment {
-    // Метод для "прогрева" JVM (JIT-компилятора)
     public void warmup() {
         Graph warmupGraph = createRandomGraph(50, 200);
         for (int i = 0; i < 100; i++) {
@@ -12,17 +11,12 @@ public class Experiment {
 
     public void runTraversals(Graph g, int size) {
         System.out.println("\nTesting graph size: " + size);
-
-        // Замер BFS
         long startBfs = System.nanoTime();
         g.bfs(0);
         long endBfs = System.nanoTime();
-
-        // Замер DFS
         long startDfs = System.nanoTime();
         g.dfs(0);
         long endDfs = System.nanoTime();
-
         System.out.println("BFS Time: " + (endBfs - startBfs) + " ns");
         System.out.println("DFS Time: " + (endDfs - startDfs) + " ns");
     }
@@ -30,7 +24,6 @@ public class Experiment {
     public void runMultipleTests() {
         int[] sizes = {10, 30, 100};
         for (int size : sizes) {
-            // Увеличиваем количество ребер до size * 5 для лучшей связности
             Graph g = createRandomGraph(size, size * 5);
             runTraversals(g, size);
         }
@@ -39,17 +32,14 @@ public class Experiment {
     private Graph createRandomGraph(int size, int edgeCount) {
         Graph g = new Graph();
         Random r = new Random();
-
         for (int i = 0; i < size; i++) {
-            g.addVertex(new Vertex(i)); // [cite: 38]
+            g.addVertex(new Vertex(i)); 
         }
 
         for (int i = 0; i < edgeCount; i++) {
             int from = r.nextInt(size);
             int to = r.nextInt(size);
-            if (from != to) {
-                g.addEdge(from, to); // [cite: 39, 63]
-            }
+            if (from != to) {g.addEdge(from, to);}
         }
         return g;
     }
