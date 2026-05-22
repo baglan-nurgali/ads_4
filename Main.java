691a04a51e6ecf6ea48d7f0e3b348126a4eb433a
baglan-nurgali <251667@astanait.edu.kz>
2026-05-22 20:40:33 +0500

    Add files via upload
    Added test cases for weighted edges to verify Dijkstra's shortest path implementation

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Experiment experiment = new Experiment();
-
+         Experiment experiment = new Experiment();
         System.out.println("Warming up JVM...");
         experiment.warmup();
-
         System.out.println("Starting experiments...");
-        experiment.runMultipleTests(); 
+        experiment.runMultipleTests();
+        
+        Graph g = new Graph();
+        g.addVertex(new Vertex(0));
+        g.addVertex(new Vertex(1));
+        g.addVertex(new Vertex(2));
+        g.addEdge(0, 1, 10);
+        g.addEdge(1, 2, 5);
+        g.addEdge(0, 2, 20);
+        g.dijkstra(0);
     }
 }
